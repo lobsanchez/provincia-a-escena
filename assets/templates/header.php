99aba515a8e2fd3f8a0777c7a66557955c8651b7
Oscar Bueno <lobsanchez@hotmail.com>
2023-12-31 02:22:48 +0100

    Rediseño del navmenu para responsive y comienzo de festival.php

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,6 +1,27 @@
 <header class="container mx-auto">
     <div class="bg-white mx auto">
-        <nav class="flex items-center mx-auto my-3 justify-between ">
+        <nav class="flex items-center mx-auto my-3 px-6 justify-between ">
+            <a href="#" class="enlace">
+                <img class="h-14 lg:h-16 xl:h-[80px]" src="assets/images/logo-provincia-low.png" width="Logotipo">
+            </a>
+            <ul class="hidden links lg:flex items-center justify-center gap-1 px-4 py-1 rounded-full menu text-menu">
+                <li class="link"><a href="festival.php"><span>EL FESTIVAL</span></a></li>
+                <li class="link"><a href="#"><span>PROVINCIA<sup class="index">A ESCENA</sup></span></a></li>
+                <li class="link"><a href="#"><span>CALENDARIO</span></a></li>
+                <li class="link"><a href="#"><span>ESCUELA<sup class="index">DE ESPECTADORES</sup></span></a></li>
+                <li class="link"><a href="#"><span>CONTACTO</span></a></li>
+            </ul>
+            <input type="checkbox" id="check" class="hidden">
+            <label for="check" class="checkbtn link lg:hidden">
+                <h3>MENÚ</h3>
+            </label>
+        </nav>
+        <section></section>
+    </div>  
+</header>
+
+        
+     <!--    
             <div class="logo p-1 ">
                 <img src="assets/images/logo-provincia-low.png" width="">
             </div>
@@ -18,3 +39,4 @@
         <section></section>
     </div>  
 </header>
+-->
---
+++ festival.php
@@ -0,0 +1,85 @@
+<?php require_once("assets/config/config.php"); ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>   <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
+<!--[if IE 7]> 		<html class="no-js lt-ie9 lt-ie8" lang="es"> <![endif]-->
+<!--[if IE 8]> 		<html class="ie8 no-js" lang="es"> <![endif]-->
+<!--[if IE 9]> 		<html class="ie9 no-js" lang="es"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="es">
+<!--<![endif]-->
+<head>
+      <meta charset="utf-8">
+      <meta name="author" content="Luis Óscar Bueno Sánchez">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="Proyecto final DIW - Provincia a escena">   
+      <title>Provincia a escena </title>    
+      <link rel="stylesheet" href="assets/css/dist/output.css" type="text/css"/>   
+      
+   </head>
+   <body class="font-roboto">
+        <?php include("assets/templates/header.php"); ?>   	
+        <main>
+          <section class="container py-12 mx-auto hero">
+                <h2 class="xl:text-[140px] lg:text-[110px] text-[60px] px-4 leading-tight font-anton "><span>EL FESTIVAL</span></h2> 
+          </section>
+        
+          <section class="pt-16 lg:flex">
+                <div class="px-16">
+                    <a class="text-4xl font-anton text-menu" href="#">PROGRAMA DE PROMOCIÓN CULTURAL</a>
+                    <p class="pt-5 text-lg text-menu">PROVINCIA A ESCENA, es un programa de promoción cultural y dinamización 
+                        socioeducativa en la provincia de Salamanca que se estructura en dos partes: un Festival Familiar de Artes 
+                        Escénicas que se desarrolla en fines de semana durante los meses de otoño, y ampara una programación 
+                        complementaria de actividades de formación de públicos en colegios e institutos en el marco de la Escuela Rural 
+                        de Espectadores/as.
+                    </p>
+                </div>
+                <div class="px-16">
+                    <a class="text-4xl font-anton text-menu" href="#">DOS LÍNEAS DE INTERVENCIÓN: RECINTOS ESCÉNICOS Y CENTROS ESCOLARES</a>
+                    <p class="pt-5 text-lg text-menu"> El IV Festival Familiar de Artes Escénicas de la provincia de Salamanca desarrollará su parte troncal del 1 de octubre 
+                        al 25 de noviembre de 2023; comenzando en los meses de abril, mayo y junio también sus acciones complementarias para 
+                        escolares. Este programa, que contribuye a facilitar el acceso a la cultura a los ciudadanos de entornos rurales, se 
+                        desglosa en dos líneas de intervención: una programación multidisciplinar en recintos escénicos de una quincena de  
+                        municipios con menos de seis mil habitantes y un conjunto de actividades escénicas y didácticas destinadas a medio 
+                        centenar de centros escolares de la provincia, con atención preferencial a los Centros Rurales Agrupados. 
+                    </p>
+                </div>
+                <div class="px-16">
+                    <a class="text-4xl font-anton text-menu" href="#">65% DE COMPAÑÍAS SALMANTINAS</a>
+                    <p class="pt-5 text-lg text-menu">El FESTIVAL PROVINCIA A ESCENA está organizado por el Área de Cultura de la Diputación Provincial de Salamanca, y cuenta con 
+                        la colaboración de la Consejería de Cultura y Turismo de la Junta de Castilla y León. Pretende acercar las artes escénicas 
+                        a todos los rincones de la provincia, fomentar la creación de nuevos públicos y estimular la participación en familia en 
+                        actividades culturales. El 80% de las compañías profesionales participantes son de Castilla y León, y el 65% de las 
+                        actuaciones serán realizadas por compañías salmantinas.  
+                    </p>
+                </div>
+          </section>
+          <img class="pt-16" src="assets/images/PEZ+LUNA_5.jpg">        
+          <section class="pt-16 lg:flex">        
+          <img class="lg:w-1/3" src="assets/images/BOMBILLA+IZQDA+++GRAFISMO.png">  
+            <div class="px-16">
+                <a class="text-4xl font-anton text-menu" href="#">UNA INVITACIÓN A CONOCER LA PROVINCIA DE SALAMANCA, SUS PUEBLOS Y SUS TEATROS</a> 
+                <p class="pt-5 text-lg text-menu">PROVINCIA A ESCENA invita a descubrir la provincia de Salamanca, sus pueblos y sus teatros. La programación 
+                escénica está integrada por algunas de las principales compañías nacionales especializadas en público infantil y 
+                familiar, cuyos espectáculos muestran un gran compromiso social y reflejan las preocupaciones de nuestro tiempo, 
+                sin renunciar al componente lúdico y divertido del ejercicio escénico.
+                </p>
+            </div>
+            <div class="px-16">
+                <a class="text-4xl font-anton text-menu" href="#">LAS ENTRADAS SON GRATUITAS HASTA COMPLETAR EL AFORO</a>
+                <p class="pt-5 text-lg text-menu">ENTRADAS: Las entradas para ver los espectáculos programados en Provincia a Escena los fines de semana, son gratuitas hasta completar el aforo. 
+                Cada espacio escénico puede gestionar la entrada del público acorde a sus dinámicas habituales de programación, ofreciendo la posibilidad de  
+                recoger invitaciones, otros sistemas alternativos y entrada libre hasta completar el aforo. Rogamos se informen con antelación en el propio teatro, 
+                y que acudan con tiempo suficiente para disfrutar de las funciones. Con esta política de gratuidad se pretende reforzar la intencionalidad de la 
+                Diputación Provincial de Salamanca de mostrar el acceso a la cultura como servicio público universal, y facilitar el acceso del público. 
+                </p>
+                <p class="pt-5 text-lg text-menu">Todos los espectáculos son para todos los públicos, especialmente recomendados para disfrutar en familia. Las representaciones están 
+                    especialmente seleccionadas para agradar a cualquier público.
+                </p>
+            </div>
+          </section>
+       
+        </main>
+        <?php include("assets/templates/footer.php"); ?> 
+        <script src="assets/js/custom.js"></script>
+   </body>   
+ </html>
